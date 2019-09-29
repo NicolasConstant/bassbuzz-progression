5f715fba4a49128e8e927d22ba1c40b95ca4141e
Nicolas Constant <github@nicolas-constant.com>
2019-09-29 02:41:40 -0400

    schedule section design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScheduleComponent } from './components/schedule/schedule.component';
+import { LessonComponent } from './components/lesson/lesson.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ScheduleComponent
+    ScheduleComponent,
+    LessonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/lesson/lesson.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-lesson',
+    templateUrl: './lesson.component.html',
+    styleUrls: ['./lesson.component.scss']
+})
+export class LessonComponent implements OnInit {
+    @Input() id: string;
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
